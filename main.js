5d3217ed071af08dae7fff9660b251b07545322e
rileyhales <rileyhales1@gmail.com>
2019-05-01 10:41:11 -0600

    change sizes for mobile friendly

--- main.js
+++ main.js
@@ -12,29 +12,11 @@ $(document).ready(function(){
 
     let i = 1;
     for (let movie in movies) {
-        if (i === 1) {
-            $("#1").append('<img class="poster" src=' + movies[movie] + '>');
-            i = i + 1;
-        }
-        else if (i === 2) {
-            $("#2").append('<img class="poster" src=' + movies[movie] + '>');
-            i = i + 1;
-        }
-        else if (i === 3) {
-            $("#3").append('<img class="poster" src=' + movies[movie] + '>');
-            i = i + 1;
-        }
-        else if (i === 4) {
-            $("#4").append('<img class="poster" src=' + movies[movie] + '>');
-            i = i + 1;
-        }
-        else if (i === 5) {
-            $("#5").append('<img class="poster" src=' + movies[movie] + '>');
-            i = i + 1;
-        }
-        else if (i === 6) {
-            $("#6").append('<img class="poster" src=' + movies[movie] + '>');
-            i = 1;
-        }
+        if (i === 1) {$("#1").append('<img class="poster" src=' + movies[movie] + '>');i = i + 1;}
+        else if (i === 2) {$("#2").append('<img class="poster" src=' + movies[movie] + '>');i = i + 1;}
+        else if (i === 3) {$("#3").append('<img class="poster" src=' + movies[movie] + '>');i = i + 1;}
+        else if (i === 4) {$("#4").append('<img class="poster" src=' + movies[movie] + '>');i = i + 1;}
+        else if (i === 5) {$("#5").append('<img class="poster" src=' + movies[movie] + '>');i = i + 1;}
+        else if (i === 6) {$("#6").append('<img class="poster" src=' + movies[movie] + '>');i = 1;}
     }
 });
