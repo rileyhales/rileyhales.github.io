376480cdffe14bb839ce29dcb0ae4e5305e68e12
rileyhales <rileyhales1@gmail.com>
2020-03-15 18:08:48 -0600

    update to themoviedatabase api and pages

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     let i = 1;
-    for (let movie in database) {
-        if (database[movie]['url'] !== '') {
-            if (i === 1) {$("#1").append('<img class="poster" src=' + database[movie]['url'] + '>');i = i + 1;}
-            else if (i === 2) {$("#2").append('<img class="poster" src=' + database[movie]['url'] + '>');i = i + 1;}
-            else if (i === 3) {$("#3").append('<img class="poster" src=' + database[movie]['url'] + '>');i = i + 1;}
-            else if (i === 4) {$("#4").append('<img class="poster" src=' + database[movie]['url'] + '>');i = i + 1;}
-            else if (i === 5) {$("#5").append('<img class="poster" src=' + database[movie]['url'] + '>');i = i + 1;}
-            else if (i === 6) {$("#6").append('<img class="poster" src=' + database[movie]['url'] + '>');i = 1;}
+    for (let movie in moviedb) {
+        console.log(movie);
+        console.log(moviedb[movie]);
+        if (moviedb[movie][1] !== '') {
+            let pg = 'https://themoviedb.org/movie/' + moviedb[movie][0];
+            let img = 'https://image.tmdb.org/t/p/w500' + moviedb[movie][1];
+            if (i === 1) {$("#1").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = i + 1;}
+            else if (i === 2) {$("#2").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = i + 1;}
+            else if (i === 3) {$("#3").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = i + 1;}
+            else if (i === 4) {$("#4").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = i + 1;}
+            else if (i === 5) {$("#5").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = i + 1;}
+            else if (i === 6) {$("#6").append('<a href=' + pg + '><img class="poster" src=' + img + '></a>');i = 1;}
         }
     }
 });
