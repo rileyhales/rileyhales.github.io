e18e34846398d82aac9b2391bb64c90cefcb4abc
rileyhales <rileyhales1@gmail.com>
2020-04-03 21:47:49 -0600

    db update writes to js

--- updatedb.py
+++ updatedb.py
@@ -3,16 +3,15 @@ import os
 import pickle
 import pandas
 import numpy
+import requests
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 
-def retrieve_csv_from_google_drive():
+def read_csv_from_google_drive(sheet_id, sheet_range, credentials_json_path):
     # the spreadsheet info
     scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    sheetID = '1IwN6augG0fm6NG8-ddhMmBrinTOpgyCnNvLKCFJA4bI'
-    sheetrange = 'MovieList!A:K'
 
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is created automatically when the
@@ -25,7 +24,7 @@ def retrieve_csv_from_google_drive():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('sheetscredentials.json', scopes)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_json_path, scopes)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
@@ -35,7 +34,7 @@ def retrieve_csv_from_google_drive():
 
     # Call the Sheets API to get the spreadsheet data
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=sheetID, range=sheetrange).execute()
+    result = sheet.values().get(spreadsheetId=sheet_id, range=sheet_range).execute()
     values = result.get('values', [])
 
     # make all the rows the same length
@@ -55,12 +54,54 @@ def retrieve_csv_from_google_drive():
     print(df.head())
     # df.to_csv('db.csv', index=False)
 
-    # format for outputting a json
-    df.index = df['Movie']
-    del df['Movie']
-    df.to_json('mvdb.json', orient='index')
-
     return df
 
 
-retrieve_csv_from_google_drive()
+def get_id_url_from_tmdb(df, tmdb_api_key):
+    movielist = list(df['Movie'])
+    fails = []
+
+    url = 'https://api.themoviedb.org/3/search/movie?'
+    for movie in movielist:
+        print(df.loc[df['Movie'] == movie, 'ID'])
+        if df.loc[df['Movie'] == movie, 'ID'] == '':
+            params = {
+                'query': movie,
+                'language': 'en-US',
+                'api_key': tmdb_api_key,
+            }
+            try:
+                data = requests.get(url, params).json()
+                df.loc[df['Movie'] == movie, 'ID'] = data['results'][0]['id']
+                df.loc[df['Movie'] == movie, 'Poster'] = data['results'][0]['poster_path']
+            except Exception:
+                fails.append(movie)
+    print(fails)
+    return df
+
+
+def update_google_drive_sheet(sheet_id, sheet_range, credentials_json_path, df):
+    return
+
+
+def df_to_json(df):
+    # format for outputting a json
+    df.index = df['Movie']
+    del df['Movie']
+    df.to_json('mvdb.js', orient='index')
+    with open('mvdb.js', 'r') as f:
+        db = f.readline()
+    with open('mvdb.js', 'w') as f:
+        f.write('mvdb=' + db + ';')
+
+
+if __name__ == '__main__':
+    sheet_id = '1IwN6augG0fm6NG8-ddhMmBrinTOpgyCnNvLKCFJA4bI'
+    sheet_range = 'MovieList!A:K'
+    credentials_json_path = 'sheetcredentials.json'
+    tmdb_api_key = ''
+
+    df = read_csv_from_google_drive(sheet_id, sheet_range, credentials_json_path)
+    # df = get_id_url_from_tmdb(df, tmdb_api_key)
+    # update_google_drive_sheet(df)
+    df_to_json(df)
