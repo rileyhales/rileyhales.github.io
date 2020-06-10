41737b3bbfa1b283a9393a53797229b3e4ae11bb
rileyhales <rileyhales1@gmail.com>
2020-06-10 00:54:31 -0600

    Add styling to recipes page

--- assets/recipes_update_workflow/recipes.py
+++ assets/recipes_update_workflow/recipes.py
@@ -1,6 +1,12 @@
+# install pandoc
+# pandoc recipes.docx -o recipes.html
+
 import os
 
 options = []
+
+base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'recipes')
+
 with open('recipes.html', 'r') as r:
     a = r.read()
     a = a.split('<h1 id="')
@@ -8,9 +14,9 @@ with open('recipes.html', 'r') as r:
         recipename = b.split('"')[0]
         if recipename == '':
             continue
-        with open(os.path.join('database', f'{recipename}.html'), 'w') as c:
+        with open(os.path.join(base_path, 'database', f'{recipename}.html'), 'w') as c:
             c.write('<h1 id="' + b)
         options.append([recipename, recipename.replace('-', ' ').title()])
-with open('../../recipes/recipes.js', 'w') as js:
+with open(os.path.join(base_path, 'recipes.js'), 'w') as js:
     js.write('let recipeOptions=' + str(options))
 print(options)
