8233684c78fe798b92036c3b1756616a36ff5f09
rileyhales <rileyhales1@gmail.com>
2020-06-10 18:23:52 -0600

    more recipes

--- assets/recipes_update_workflow/recipes.py
+++ assets/recipes_update_workflow/recipes.py
@@ -7,7 +7,7 @@ options = []
 
 base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'recipes')
 
-with open('recipes.html', 'r') as r:
+with open(os.path.join(os.path.dirname(__file__), 'recipes.html'), 'r') as r:
     a = r.read()
     a = a.split('<h1 id="')
     for b in a:
@@ -17,6 +17,7 @@ with open('recipes.html', 'r') as r:
         with open(os.path.join(base_path, 'database', f'{recipename}.html'), 'w') as c:
             c.write('<h1 id="' + b)
         options.append([recipename, recipename.replace('-', ' ').title()])
+options.sort(key=lambda x: x[0])
 with open(os.path.join(base_path, 'recipes.js'), 'w') as js:
     js.write('let recipeOptions=' + str(options))
 print(options)
